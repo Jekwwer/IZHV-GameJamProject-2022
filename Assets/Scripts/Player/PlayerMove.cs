67df7a0939a0903fd7fd7b27d82581eba78301a4
Evgenii Shiliaev <xshili00@stud.fit.vutbr.cz>
2022-01-15 17:13:12 +0100

    Added basic controls ansd character moving

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float movementSpeed = 3f;
+    public float leftRightMovementSpeed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed, Space.World);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (this.gameObject.transform.position.x > LevelBoundary.leftSide)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * leftRightMovementSpeed);
+            }
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * leftRightMovementSpeed);
+            }
+        }
+    }
+}
